a665707778689f9b08b2a8172d2fb086b6bb20e3
nicarrh <ncarrh@gmail.com>
2026-03-10 16:40:36 -0300

    feat: (App) add Toast and Modal

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import { RootNavigator } from '@navigation';
 import { NavigationContainer } from '@react-navigation/native';
 import { NavigationTheme } from '@shared';
 import React from 'react';
+import Toast from 'react-native-toast-message';
 import { AuthProvider } from './features/auth/hooks/useAuth';
+import ModalTotp from './features/auth/presentation/components/ModalTotp';
 
 export default function App() {
 	return (
 		<NavigationContainer theme={NavigationTheme}>
 			<AuthProvider>
 				<RootNavigator />
+				<ModalTotp />
 			</AuthProvider>
+			<Toast position='bottom' />
 		</NavigationContainer>
 	);
 }
