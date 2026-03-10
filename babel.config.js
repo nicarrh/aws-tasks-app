cf4d18b4ea298bbd0bcda99f121990ee6f4a13fd
ncarrenoh <nacarrenohex@bice.cl>
2026-03-10 10:06:05 -0300

    feat: [config] add project configuration

---
+++ babel.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+	presets: ['babel-preset-expo'],
+	plugins: [
+		[
+			'module-resolver',
+			{
+				root: ['./src'],
+				alias: {
+					'@navigation': './src/app/navigation/index.ts',
+					'@auth': './src/features/auth/index.ts',
+					'@tasks': './src/features/tasks/index.ts',
+					'@profile': './src/features/profile/index.ts',
+					'@shared': './src/shared/index.ts',
+					'@users': './src/features/users/index.ts',
+				},
+			},
+		],
+		[
+			'module:react-native-dotenv',
+			{
+				moduleName: '@env',
+				path: '.env',
+				safe: false,
+				allowUndefined: true,
+			},
+		],
+	],
+};
