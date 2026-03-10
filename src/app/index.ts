e7e489b909d9b76372cf0d2e35019107671f272d
ncarrenoh <nacarrenohex@bice.cl>
2026-03-10 10:12:23 -0300

    feat: (app) expose content from app feature

---
+++ src/app/index.ts
@@ -0,0 +1,5 @@
+import { NavigationTypes } from './navigation';
+import RootNavigator from './navigation/RootNavigator';
+import Splash from './presentation/screens/Splash';
+export { Splash as AppSplash, NavigationTypes, RootNavigator };
+
