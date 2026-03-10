e5b295aaef06169cff7199374afe356d1707510b
nicarrh <ncarrh@gmail.com>
2026-03-10 17:12:46 -0300

    feat: (auth) export screens

--- src/features/auth/index.ts
+++ src/features/auth/index.ts
@@ -1,7 +1,16 @@
 import { useAuth } from './hooks/useAuth';
 import { getValidAccessToken } from './infraestructure/auth.service';
 import { ConfirmCodeScreen } from './presentation/screens/confirm-code-screen/ConfirmCodeScreen';
+import ConfirmTotpScreen from './presentation/screens/confirm-totp/ConfirmTotp';
 import LoginScreen from './presentation/screens/login-screen/LoginScreen';
 import RegisterScreen from './presentation/screens/register-screen/RegisterScreen';
+import SetupTotpScreen from './presentation/screens/setup-totp/SetupTotp';
 
-export { ConfirmCodeScreen, getValidAccessToken, LoginScreen, RegisterScreen, useAuth };
+export {
+  ConfirmCodeScreen,
+  ConfirmTotpScreen,
+  getValidAccessToken,
+  LoginScreen,
+  RegisterScreen, SetupTotpScreen, useAuth
+};
+
